045217c41eaf45dca54afb5ea011bbb6fd0a7924
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2025-02-24 11:18:00 +0700

    Added auth service

---
+++ auth-service/src/test/java/com/tien4112004/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tien4112004.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
