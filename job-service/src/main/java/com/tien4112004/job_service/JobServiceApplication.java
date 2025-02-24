f23d8bd35e6b8fc5f6d8f6f94a37c7133a1667be
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2025-02-24 11:15:04 +0700

    Added job service (template)

---
+++ job-service/src/main/java/com/tien4112004/job_service/JobServiceApplication.java
@@ -0,0 +1,14 @@
+package com.tien4112004.job_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobServiceApplication.class, args);
+	}
+
+}
+			
