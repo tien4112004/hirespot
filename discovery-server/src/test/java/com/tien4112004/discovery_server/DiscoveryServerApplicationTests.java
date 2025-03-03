a963c3471a2a28a5f87b1802fe001f95f5f17851
Thanh Tien Phan <64484799+tien4112004@users.noreply.github.com>
2025-03-03 21:34:59 +0700

    Fixed spelling in Eureka configuration

---
+++ discovery-server/src/test/java/com/tien4112004/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tien4112004.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
